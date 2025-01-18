02fc9d757c291bb56ee2a8058f87e256b030cc9a
FriedHamm <kwh2716@gmail.com>
2025-01-18 14:22:05 +0900

    ㄴㄴ

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-import Image from "next/image";
-import TextEditor from "@/components/texteditor/TextEditor";
 import HeroSection from "@/components/main page/HeroSection";
 import CourseSection from "@/components/main page/CourseSection";
-import {router} from "next/client";
+
 
 export default function Home({searchParams}) {
   const code = searchParams?.code;
   const state = searchParams?.state;
-
-
+  
   // 만약에, redirect 같은게 넘어오면, 얘는 그걸 처리를 못해요.
 
   if (code && state) {
