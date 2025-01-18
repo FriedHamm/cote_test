4224f207fc5efe43ce987487368da71810e54e63
FriedHamm <kwh2716@gmail.com>
2025-01-18 13:04:26 +0900

    4

--- src/app/account/login/page.jsx
+++ src/app/account/login/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <a href="https://nossidev.run.goorm.site/account/social-login?provider=kakao&next=/">
+      <a href="https://nossidev.run.goorm.site/account/social-login?provider=kakao">
         <Image
           src="/img/kakao_login_medium_narrow.png"
           alt="login_logo"
