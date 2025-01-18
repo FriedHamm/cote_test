43cd0ca441cfe7817d2178b19f192fbe0ac38a6b
FriedHamm <kwh2716@gmail.com>
2025-01-18 14:26:42 +0900

    ㄴㅇㅁㄴㅇㅁㄴㅇ

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 import HeroSection from "@/components/main page/HeroSection";
 import CourseSection from "@/components/main page/CourseSection";
+import {redirect} from "next/navigation";
 
 
 export default function Home({searchParams}) {
   const code = searchParams?.code;
   const state = searchParams?.state;
-  
+
   // 만약에, redirect 같은게 넘어오면, 얘는 그걸 처리를 못해요.
 
   if (code && state) {
-    location.href = `https://nossidev.run.goorm.site/account/social-login/callback/?code=${code}&state=${state}`
+    redirect(`https://nossidev.run.goorm.site/account/social-login/callback/?code=${code}&state=${state}`);
   } else {
     console.log('code 파라미터가 URL에 없습니다.');
   }
