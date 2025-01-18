c2094a575ff6e1a5dba9d5bdfa933504372e7f60
FriedHamm <kwh2716@gmail.com>
2025-01-18 14:18:53 +0900

    sdasd

--- src/app/page.js
+++ src/app/page.js
@@ -2,24 +2,17 @@ import Image from "next/image";
 import TextEditor from "@/components/texteditor/TextEditor";
 import HeroSection from "@/components/main page/HeroSection";
 import CourseSection from "@/components/main page/CourseSection";
+import {router} from "next/client";
 
 export default function Home({searchParams}) {
   const code = searchParams?.code;
   const state = searchParams?.state;
 
-  if (code) {
-    // code 파라미터가 있을 경우, 지정된 URL로 요청 보내기
-    fetch(`https://nossidev.run.goorm.site/account/social-login/callback/?code=${code}&state=${state}`, {
-      method: 'GET', // 필요에 따라 POST 등으로 변경
-      // 추가 옵션 설정 가능 (헤더, body 등)
-    })
-      .then(() => {
-        console.log('요청완료');
-        // 응답 데이터 처리 로직 추가
-      })
-      .catch(error => {
-        console.error('요청 중 오류 발생:', error);
-      });
+
+  // 만약에, redirect 같은게 넘어오면, 얘는 그걸 처리를 못해요.
+
+  if (code && state) {
+    location.href = `https://nossidev.run.goorm.site/account/social-login/callback/?code=${code}&state=${state}`
   } else {
     console.log('code 파라미터가 URL에 없습니다.');
   }
