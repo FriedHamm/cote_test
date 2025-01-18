b22c027eed63eef6be384fe31174292a39967d7e
FriedHamm <kwh2716@gmail.com>
2025-01-18 13:46:14 +0900

    ㄴ

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,10 @@ import CourseSection from "@/components/main page/CourseSection";
 
 export default function Home({searchParams}) {
   const code = searchParams?.code;
-  
+
   if (code) {
     // code 파라미터가 있을 경우, 지정된 URL로 요청 보내기
-    fetch(`https://nossidev.run.goorm.site/account/social-login/callback/?code=${encodeURIComponent(code)}`, {
+    fetch(`https://nossidev.run.goorm.site/account/social-login/callback/?code=${code}`, {
       method: 'GET', // 필요에 따라 POST 등으로 변경
       // 추가 옵션 설정 가능 (헤더, body 등)
     })
