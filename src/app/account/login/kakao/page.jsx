ca96afe6f1c0df2e8668ab1bfb7b16182f8e0cae
FriedHamm <kwh2716@gmail.com>
2025-01-20 21:06:05 +0900

    test7

--- src/app/account/login/kakao/page.jsx
+++ src/app/account/login/kakao/page.jsx
@@ -18,7 +18,7 @@ function KakaoCallbackHandler() {
     axios.defaults.withCredentials = true;
     axios
       .get(
-        `/api/account/social-login/callback/?code=${code}&state=${state}`,
+        `https://nossidev.run.goorm.site/account/social-login/callback/?code=${code}&state=${state}`,
         {
           withCredentials: true, // 쿠키 주고받기 설정
         }
