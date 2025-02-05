50b0a3f0384e0b7a568a93c2b89fe0a79dd8be47
FriedHamm <kwh2716@gmail.com>
2025-02-05 14:59:26 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/backend/:path*',
-        destination: 'https://nossidev.run.goorm.site/:path*'
-      }
-    ]
-  }
 };
 
 export default nextConfig;
