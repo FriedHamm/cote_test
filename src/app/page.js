da055e94606927a90121f363317154e5e3c74d79
FriedHamm <kwh2716@gmail.com>
2025-02-05 17:21:12 +0900

    test1

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,20 @@ import { useDispatch } from 'react-redux';
 import HeroSection from '@/components/main page/HeroSection';
 import CourseSection from '@/components/main page/CourseSection';
 import {checkAuth} from "@/store/slices/authSlice";
+import api from "@/axiosConfig";
 
 export default function Home() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(checkAuth());
+    api.post('account/v1/auth/social/token/refreshment')
+      .then(() => {
+        console.log('토큰 리프레시 요청이 성공했습니다.');
+      })
+      .catch(err => {
+        console.log('토큰 리프레시 요청에 실패했습니다.', err);
+      });
   }, [dispatch]);
 
   return (
